9729a7364f8ce5cd0d107574e8186cb151c18463
Rosemilson <rozbr96@gmail.com>
2025-03-23 21:10:29 -0300

    enable send selected to quickfix list and open it

--- .config/nvim/lua/core/plugins/config/telescope.lua
+++ .config/nvim/lua/core/plugins/config/telescope.lua
@@ -1,6 +1,14 @@
 
+local actions = require('telescope.actions')
+
 require('telescope').setup {
-  defaults = {},
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-S-q>'] = actions.send_selected_to_qflist + actions.open_qflist
+      }
+    }
+  },
 
   pickers = {
     find_files = {
@@ -8,4 +16,3 @@ require('telescope').setup {
     }
   }
 }
-
