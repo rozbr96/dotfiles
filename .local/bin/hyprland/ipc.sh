b66cc9032987566f96ee2f72b33e1ca3acc3fe35
Rosemilson <rozbr96@gmail.com>
2025-07-24 02:02:51 -0300

    [hyprland] track keyboard layout according to active window

---
+++ .local/bin/hyprland/ipc.sh
@@ -0,0 +1,37 @@
+
+declare -A active_layouts
+layouts=("English (US)" "Portuguese (Brazil)")
+active_window=""
+
+handle() {
+  echo "$1" | awk -F ">>" '{ print $1 " " $2 }' | read -r event data
+
+  case $event in
+    activewindowv2)
+      active_window=$data
+      active_window_layout=$active_layouts[$active_window]
+
+      if [ ! -z $active_window_layout ]; then
+        hyprctl switchxkblayout all $active_window_layout
+      fi
+    ;;
+
+    activelayout)
+      layout="$(echo $data | cut -d',' -f2)"
+      layout_index=${layouts[(Ie)$layout]}
+
+      if [ $layout_index -gt 0 ]; then
+        layout_index=$(( $layout_index - 1 ))
+        active_layouts[$active_window]=$layout_index
+      fi
+    ;;
+
+    *)
+      echo "Unknown Event: $event"
+      echo "Input: $1"
+      echo ""
+    ;;
+  esac
+}
+
+socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do handle "$line"; done
