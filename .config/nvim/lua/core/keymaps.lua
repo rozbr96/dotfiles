946ddcb598d245894e01b0be6913e37004f79e7e
Rosemilson <rozbr96@gmail.com>
2025-08-07 17:13:25 -0300

    [nvim] add keybindings for specs runner

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,104 +1,109 @@
-
 vim.g.mapleader = '\\'
 
 local bindings_per_mode = {
   ['n'] = {
     -- general config
-    {keys = '<leader>r', command = ':lua ReloadConfig()<CR>'},
-    {keys = '<leader>T', command = ':terminal<CR>'},
-    {keys = '<F3>', command = ':noh<CR>'},
+    { keys = '<leader>r',         command = ':lua ReloadConfig()<CR>' },
+    { keys = '<leader>T',         command = ':terminal<CR>' },
+    { keys = '<F3>',              command = ':noh<CR>' },
 
     -- saving
-    {keys = '<leader>ww', command = ':w<CR>'},
-    {keys = '<leader>wa', command = ':wa<CR><ESC>'},
-    {keys = '<leader>Ww', command = ':w!<CR>'},
-    {keys = '<leader>Wa', command = ':wa!<CR>'},
-    {keys = '<leader>wq', command = ':wq<CR>'},
-    {keys = '<leader>Wq', command = ':wq!<CR>'},
+    { keys = '<leader>ww',        command = ':w<CR>' },
+    { keys = '<leader>wa',        command = ':wa<CR><ESC>' },
+    { keys = '<leader>Ww',        command = ':w!<CR>' },
+    { keys = '<leader>Wa',        command = ':wa!<CR>' },
+    { keys = '<leader>wq',        command = ':wq<CR>' },
+    { keys = '<leader>Wq',        command = ':wq!<CR>' },
 
     -- quiting
-    {keys = '<leader>qq', command = ':q<CR>'},
-    {keys = '<leader>qa', command = ':qa<CR>'},
-    {keys = '<leader>Qq', command = ':q!<CR>'},
-    {keys = '<leader>Qa', command = ':qa!<CR>'},
+    { keys = '<leader>qq',        command = ':q<CR>' },
+    { keys = '<leader>qa',        command = ':qa<CR>' },
+    { keys = '<leader>Qq',        command = ':q!<CR>' },
+    { keys = '<leader>Qa',        command = ':qa!<CR>' },
 
     -- tree
-    {keys = '<leader>tt', command = ':NvimTreeToggle<CR>'},
-    {keys = '<leader>tf', command = ':NvimTreeFindFile!<CR>'},
-    {keys = '<Tab>', command = ':NvimTreeFocus<CR>'},
+    { keys = '<leader>tt',        command = ':NvimTreeToggle<CR>' },
+    { keys = '<leader>tf',        command = ':NvimTreeFindFile!<CR>' },
+    { keys = '<Tab>',             command = ':NvimTreeFocus<CR>' },
 
     -- telescope
-    {keys = '<leader>ff', command = ':Telescope find_files<CR>'},
-    {keys = '<leader>fh', command = ':Telescope search_history<CR>'},
-    {keys = '<leader>fg', command = ':Telescope live_grep<CR>'},
-    {keys = '<leader>fs', command = ':Telescope live_grep<CR>'},
-    {keys = '<leader>fw', command = ':Telescope grep_string<CR>'},
-    {keys = '<leader>fb', command = ':Telescope buffers<CR>'},
+    { keys = '<leader>ff',        command = ':Telescope find_files<CR>' },
+    { keys = '<leader>fh',        command = ':Telescope search_history<CR>' },
+    { keys = '<leader>fg',        command = ':Telescope live_grep<CR>' },
+    { keys = '<leader>fs',        command = ':Telescope live_grep<CR>' },
+    { keys = '<leader>fw',        command = ':Telescope grep_string<CR>' },
+    { keys = '<leader>fb',        command = ':Telescope buffers<CR>' },
 
     -- commands
-    {keys = '<leader>cc', command = ':Telescope commands<CR>'},
-    {keys = '<leader>ch', command = ':Telescope command_history<CR>'},
-    {keys = '<leader>cm', command = ':Telescope man_pages<CR>'},
+    { keys = '<leader>cc',        command = ':Telescope commands<CR>' },
+    { keys = '<leader>ch',        command = ':Telescope command_history<CR>' },
+    { keys = '<leader>cm',        command = ':Telescope man_pages<CR>' },
 
     -- buffers
-    {keys = '<leader>vv', command = ':split<CR>'},
-    {keys = '<leader>hh', command = ':vsplit<CR>'},
-    {keys = '<leader>#', command = ':#wincmd w<CR>'},
-    {keys = '<leader><leader>#', command = '#gt <CR>'},
-    {keys = '<leader><leader>o', command = ':tab split <CR>'},
+    { keys = '<leader>vv',        command = ':split<CR>' },
+    { keys = '<leader>hh',        command = ':vsplit<CR>' },
+    { keys = '<leader>#',         command = ':#wincmd w<CR>' },
+    { keys = '<leader><leader>#', command = '#gt <CR>' },
+    { keys = '<leader><leader>o', command = ':tab split <CR>' },
 
     -- code
-    {keys = '<leader>Cp', command = ':Telescope treesitter<CR>'},
-    {keys = '<leader>Cf', command = ':Telescope lsp_references<CR>'},
-    {keys = '<leader>CD', command = ':Telescope diagnostics<CR>'},
-    {keys = '<leader>Cd', command = ':Telescope lsp_definitions<CR>'},
-    {keys = '<leader>Ci', command = ':Telescope lsp_implementations<CR>'},
-    {keys = '<leader>Ct', command = ':Telescope lsp_type_definitions<CR>'},
-    {keys = '<leader>Cs', command = ':Telescope lsp_document_symbols<CR>'},
-    {keys = '<leader>CS', command = ':Telescope lsp_workspace_symbols<CR>'},
-    {keys = '<leader>CI', command = ':Telescope lsp_incoming_calls<CR>'},
-    {keys = '<leader>CO', command = ':Telescope lsp_outgoing_calls<CR>'},
+    { keys = '<leader>Cp',        command = ':Telescope treesitter<CR>' },
+    { keys = '<leader>Cf',        command = ':Telescope lsp_references<CR>' },
+    { keys = '<leader>CD',        command = ':Telescope diagnostics<CR>' },
+    { keys = '<leader>Cd',        command = ':Telescope lsp_definitions<CR>' },
+    { keys = '<leader>Ci',        command = ':Telescope lsp_implementations<CR>' },
+    { keys = '<leader>Ct',        command = ':Telescope lsp_type_definitions<CR>' },
+    { keys = '<leader>Cs',        command = ':Telescope lsp_document_symbols<CR>' },
+    { keys = '<leader>CS',        command = ':Telescope lsp_workspace_symbols<CR>' },
+    { keys = '<leader>CI',        command = ':Telescope lsp_incoming_calls<CR>' },
+    { keys = '<leader>CO',        command = ':Telescope lsp_outgoing_calls<CR>' },
 
     -- git
-    {keys = '<leader>gl', command = ':Telescope git_commits<CR>'},
-    {keys = '<leader>gs', command = ':Telescope git_status<CR>'},
-    {keys = '<leader>gb', command = ':Telescope git_branches<CR>'},
-    {keys = '<leader>gf', command = ':Telescope git_bcommits<CR>'},
-    {keys = '<leader>ga', command = ':!git add -f %<CR>'},
-    {keys = '<leader>gu', command = ':!git unstage %<CR>'},
+    { keys = '<leader>gl',        command = ':Telescope git_commits<CR>' },
+    { keys = '<leader>gs',        command = ':Telescope git_status<CR>' },
+    { keys = '<leader>gb',        command = ':Telescope git_branches<CR>' },
+    { keys = '<leader>gf',        command = ':Telescope git_bcommits<CR>' },
+    { keys = '<leader>ga',        command = ':!git add -f %<CR>' },
+    { keys = '<leader>gu',        command = ':!git unstage %<CR>' },
 
     -- multiple cursors
-    {keys = '<A-S-Down>', command = ':MultipleCursorsAddDown<CR>'},
-    {keys = '<A-S-Up>', command = ':MultipleCursorsAddUp<CR>'},
-    {keys = '<C-LeftMouse>', command = ':MultipleCursorsMouseAddDelete<CR>'},
-    {keys = '<C-D>', command = ':MultipleCursorsAddJumpNextMatch<CR>'},
-    {keys = '<C-S-D>', command = ':MultipleCursorsAddJumpPrevMatch<CR>'},
+    { keys = '<A-S-Down>',        command = ':MultipleCursorsAddDown<CR>' },
+    { keys = '<A-S-Up>',          command = ':MultipleCursorsAddUp<CR>' },
+    { keys = '<C-LeftMouse>',     command = ':MultipleCursorsMouseAddDelete<CR>' },
+    { keys = '<C-D>',             command = ':MultipleCursorsAddJumpNextMatch<CR>' },
+    { keys = '<C-S-D>',           command = ':MultipleCursorsAddJumpPrevMatch<CR>' },
 
     -- defaults
-    {keys = '<C-i>', command = '<C-i>'},
+    { keys = '<C-i>',             command = '<C-i>' },
 
     -- move current line
-    {keys = '<A-j>', command = ":m .+1<CR>=="},
-    {keys = '<A-Down>', command = ":m .+1<CR>=="},
-    {keys = '<A-k>', command = ":m .-2<CR>=="},
-    {keys = '<A-Up>', command = ":m .-2<CR>=="},
+    { keys = '<A-j>',             command = ":m .+1<CR>==" },
+    { keys = '<A-Down>',          command = ":m .+1<CR>==" },
+    { keys = '<A-k>',             command = ":m .-2<CR>==" },
+    { keys = '<A-Up>',            command = ":m .-2<CR>==" },
+
+    -- specs runner
+    { keys = '<leader>ss',        command = ':RunSpecs<CR>' },
+    { keys = '<leader>sc',        command = ':RunCurrentSpec<CR>' },
+    { keys = '<leader>sl',        command = ':RunLastSpecCommand<CR>' },
+    { keys = '<leader>sd',        command = ':DisplayLastSpecCommandOutput<CR>' },
   },
 
   ['v'] = {
     -- indentation
-    {keys = '>', command = '>gv'},
-    {keys = '<Tab>', command = '>gv'},
-    {keys = '<', command = '<gv'},
-    {keys = '<S-Tab>', command = '<gv'},
+    { keys = '>',          command = '>gv' },
+    { keys = '<Tab>',      command = '>gv' },
+    { keys = '<',          command = '<gv' },
+    { keys = '<S-Tab>',    command = '<gv' },
 
     -- telescope
-    {keys = '<leader>fw', command = [["zy:Telescope live_grep default_text=<C-r>z<cr>]]},
+    { keys = '<leader>fw', command = [["zy:Telescope live_grep default_text=<C-r>z<cr>]] },
 
     -- move selected block
-    {keys = '<A-j>', command = ":m '>+1<CR>gv=gv"},
-    {keys = '<A-Down>', command = ":m '>+1<CR>gv=gv"},
-    {keys = '<A-k>', command = ":m '<-2<CR>gv=gv"},
-    {keys = '<A-Up>', command = ":m '<-2<CR>gv=gv"},
+    { keys = '<A-j>',      command = ":m '>+1<CR>gv=gv" },
+    { keys = '<A-Down>',   command = ":m '>+1<CR>gv=gv" },
+    { keys = '<A-k>',      command = ":m '<-2<CR>gv=gv" },
+    { keys = '<A-Up>',     command = ":m '<-2<CR>gv=gv" },
   },
 }
 
