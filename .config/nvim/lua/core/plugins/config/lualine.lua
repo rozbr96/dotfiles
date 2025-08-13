6b1406cb23d73175ad53713154f6c64e1984bc18
Rosemilson <rozbr96@gmail.com>
2025-08-13 16:30:25 -0300

    [nvim] improve lualine

--- .config/nvim/lua/core/plugins/config/lualine.lua
+++ .config/nvim/lua/core/plugins/config/lualine.lua
@@ -1,12 +1,14 @@
-
-require'lualine'.setup {
+require 'lualine'.setup {
   options = {
     theme = 'nightfly'
   },
   tabline = {
-    lualine_a = { 'filename' },
+    lualine_a = { 'vim.fn.expand("%:.")' },
     lualine_b = { 'lsp_status' },
     lualine_y = { 'tabs' },
     lualine_z = { 'datetime' },
+  },
+  inactive_sections = {
+    lualine_x = { 'location', 'winnr' }
   }
 }
