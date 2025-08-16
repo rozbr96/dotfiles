f738023c6c3249f05b5207e2f5b5893c4b689f02
Rosemilson <rozbr96@gmail.com>
2025-08-16 17:25:33 -0300

    [nvim] resolve diagnostics errors/warnings

--- .config/nvim/lua/core/plugins/config/multiple-cursors.lua
+++ .config/nvim/lua/core/plugins/config/multiple-cursors.lua
@@ -1,22 +1,22 @@
-
+local autopairs = require('core.plugins.config.autopairs')
 local cursors = require('multiple-cursors')
+local cmp = require('cmp')
 
 cursors.setup {
   custom_key_maps = {
-    {'n', '<Leader>a', function() cursors.align() end},
-    {{'n', 'i'}, 'Cc', function() vim.cmd('normal gcc') end},
-    {'v', 'Cc', function() vim.cmd('normal gc') end},
-    {{'n', 'i'}, '<C-/>', function() vim.cmd('normal gcc') end},
-    {'v', '<C-/>', function() vim.cmd('normal gc') end},
+    { { 'n', 'i' }, 'Cc',    function() vim.cmd('normal gcc') end },
+    { 'v',          'Cc',    function() vim.cmd('normal gc') end },
+    { { 'n', 'i' }, '<C-/>', function() vim.cmd('normal gcc') end },
+    { 'v',          '<C-/>', function() vim.cmd('normal gc') end },
   },
 
   pre_hook = function()
-    require('nvim-autopairs').disable()
-    require('cmp').setup({ enabled = false })
+    autopairs.disable()
+    cmp.setup({ enabled = false })
   end,
 
   post_hook = function()
-    require('nvim-autopairs').enable()
-    require('cmp').setup({ enabled = true })
+    autopairs.enable()
+    cmp.setup({ enabled = true })
   end
 }
