bef88122d87356d8f4882b926b6fb6e7d77b5351
Rosemilson <rozbr96@gmail.com>
2025-08-22 01:44:20 -0300

    [home] fonts

--- nixos/home/fonts.nix
+++ nixos/home/fonts.nix
@@ -1,3 +1,15 @@
+{ pkgs, ... }:
+
 {
-  fonts.fontconfig.enable = true;
+  home.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+    google-fonts
+    nerd-fonts.fira-code
+    noto-fonts
+    noto-fonts-emoji
+    orbitron
+  ];
+
+  fonts.fontconfig.enable = true;   # ensures fonts are visible in Ubuntu/Kitty
 }
