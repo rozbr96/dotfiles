9b69fdf003ecdd67768d01dbd316bf27bd9d7a73
Rosemilson <rozbr96@gmail.com>
2025-09-06 18:19:45 -0300

    [nvim] add which-key

--- .config/nvim/lua/core/plugins/init.lua
+++ .config/nvim/lua/core/plugins/init.lua
@@ -72,7 +72,25 @@ require("lazy").setup({
       global_keymaps_prefix = " "
     }
   },
-  { "shortcuts/no-neck-pain.nvim", version = "*" }
+  { "shortcuts/no-neck-pain.nvim", version = "*" },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  }
 })
 
 require("core.plugins.config")
