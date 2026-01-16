4870a51dc0b84871a2a4e9a231d912cf431d0358
Rosemilson <rozbr96@gmail.com>
2026-01-16 20:04:09 -0300

    [nvim] lualine

--- .config/nvim/lua/core/plugins/config/lualine.lua
+++ .config/nvim/lua/core/plugins/config/lualine.lua
@@ -1,14 +1,60 @@
 require 'lualine'.setup {
   options = {
-    theme = 'nightfly'
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+      refresh_time = 16, -- ~60fps
+      events = {
+        'WinEnter',
+        'BufEnter',
+        'BufWritePost',
+        'SessionLoadPost',
+        'FileChangedShellPost',
+        'VimResized',
+        'Filetype',
+        'CursorMoved',
+        'CursorMovedI',
+        'ModeChanged',
+      },
+    }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'vim.fn.expand("%:.")' },
+    lualine_x = { 'location', 'winnr' },
+    lualine_y = {},
+    lualine_z = {}
   },
   tabline = {
-    lualine_a = { 'vim.fn.expand("%:.")' },
-    lualine_b = { 'lsp_status' },
+    lualine_a = { 'vim.fn.fnamemodify(vim.fn.getcwd(), ":t")' },
+    lualine_b = { 'filename' },
+    lualine_c = { 'lsp_status' },
     lualine_y = { 'tabs' },
     lualine_z = { 'datetime' },
   },
-  inactive_sections = {
-    lualine_x = { 'location', 'winnr' }
-  }
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
 }
