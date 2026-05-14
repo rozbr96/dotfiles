47b559219d3aa84f51941adc2db9ec4cbb1d2b46
Rosemilson <rozbr96@gmail.com>
2026-05-14 08:46:18 -0300

    [hypr] startup

--- .config/hypr/hyprland.d/startup.lua
+++ .config/hypr/hyprland.d/startup.lua
@@ -4,13 +4,15 @@ hl.on('hyprland.start', function()
     hl.exec_cmd('hyprctl plugin load ' .. hy3_path)
   end
 
-  hl.exec_cmd('systemctl --user restart hypr-ipc')
-  hl.exec_cmd('systemctl --user restart eww')
-  hl.exec_cmd('systemctl --user restart network-monitor')
-  hl.exec_cmd('systemctl --user restart volume-monitor')
-
   hl.exec_cmd('mako')
   hl.exec_cmd('wl-paste --type text --watch cliphist store')
   hl.exec_cmd('wl-paste --type image --watch cliphist store')
   hl.exec_cmd('dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP')
+end)
+
+hl.on('config.reloaded', function()
+  hl.exec_cmd('systemctl --user restart hypr-ipc')
+  hl.exec_cmd('systemctl --user restart eww')
+  hl.exec_cmd('systemctl --user restart network-monitor')
+  hl.exec_cmd('systemctl --user restart volume-monitor')
 end)
