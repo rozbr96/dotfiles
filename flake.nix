9c9a29c97214cb29affabd0352f64a62103a31e9
Rosemilson <rozbr96@gmail.com>
2026-05-14 05:49:03 -0300

    [hypr] initial config migration one on one to v0.55.1

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    hyprland.url = "github:hyprwm/Hyprland/v0.55.0";
+    hyprland.url = "github:hyprwm/Hyprland/v0.55.1";
 
     hy3 = {
       url = "github:outfoxxed/hy3/8f56e6ea73db758152b117d507197c46c629afa8";
