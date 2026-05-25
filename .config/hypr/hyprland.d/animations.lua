48c31d4d35477e5ad4fdad05dbd46acd521953f8
Rosemilson <rozbr96@gmail.com>
2026-05-25 08:28:33 -0300

    [hypr] animations

--- .config/hypr/hyprland.d/animations.lua
+++ .config/hypr/hyprland.d/animations.lua
@@ -1,7 +1,32 @@
-hl.curve('myBezier', { type = 'bezier', points = { { 0.05, 0.9 }, { 0.1, 1.05 } } })
-hl.animation({ leaf = 'windows', enabled = true, speed = 7, bezier = 'myBezier' })
-hl.animation({ leaf = 'windowsOut', enabled = true, speed = 7, bezier = 'default', style = 'popin 80%' })
-hl.animation({ leaf = 'border', enabled = true, speed = 10, bezier = 'default' })
-hl.animation({ leaf = 'borderangle', enabled = true, speed = 8, bezier = 'default' })
-hl.animation({ leaf = 'fade', enabled = true, speed = 7, bezier = 'default' })
-hl.animation({ leaf = 'workspaces', enabled = true, speed = 6, bezier = 'default' })
+hl.config({ animations = { enabled = true } })
+
+hl.curve('md3_decel', { type = 'bezier', points = { { 0.05, 0.7 }, { 0.1, 1 } } })
+hl.curve('md3_accel', { type = 'bezier', points = { { 0.3, 0 }, { 0.8, 0.15 } } })
+hl.curve('menu_decel', { type = 'bezier', points = { { 0.1, 1 }, { 0, 1 } } })
+hl.curve('menu_accel', { type = 'bezier', points = { { 0.38, 0.04 }, { 1, 0.07 } } })
+
+hl.curve('spring_menu', { type = 'spring', mass = 1, stiffness = 80, dampening = 14 })
+hl.curve('spring_window', { type = 'spring', mass = 1, stiffness = 30, dampening = 8 })
+hl.curve('spring_open', { type = 'spring', mass = 1, stiffness = 30, dampening = 8 })
+hl.curve('spring_workspace', { type = 'spring', mass = 1.2, stiffness = 30, dampening = 10 })
+hl.curve('spring_special', { type = 'spring', mass = 1, stiffness = 30, dampening = 8 })
+
+hl.animation({ leaf = 'windows', enabled = true, speed = 1, spring = 'spring_window' })
+hl.animation({ leaf = 'windowsIn', enabled = true, speed = 1, spring = 'spring_open', style = 'gnomed' })
+hl.animation({ leaf = 'windowsOut', enabled = true, speed = 3, bezier = 'md3_accel', style = 'gnomed' })
+hl.animation({ leaf = 'border', enabled = false })
+hl.animation({ leaf = 'borderangle', enabled = false })
+hl.animation({ leaf = 'fade', enabled = true, speed = 3, bezier = 'md3_decel' })
+hl.animation({ leaf = 'zoomFactor', enabled = true, speed = 6, bezier = 'md3_decel' })
+hl.animation({ leaf = 'layersIn', enabled = true, speed = 3, spring = 'spring_menu', style = 'popin' })
+hl.animation({ leaf = 'layersOut', enabled = true, speed = 1.6, bezier = 'menu_accel', style = 'popin' })
+hl.animation({ leaf = 'fadeLayersIn', enabled = true, speed = 2, bezier = 'menu_decel' })
+hl.animation({ leaf = 'fadeLayersOut', enabled = true, speed = 1.6, bezier = 'menu_accel' })
+hl.animation({ leaf = 'workspaces', enabled = true, speed = 1, spring = 'spring_workspace', style = 'slide' })
+hl.animation({
+  leaf = 'specialWorkspace',
+  enabled = true,
+  speed = 1,
+  spring = 'spring_special',
+  style = 'slidefadevert 40%'
+})
