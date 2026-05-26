850a780e7759e0f7f3bec39151b3356473d251c2
Rosemilson <rozbr96@gmail.com>
2026-05-25 21:26:05 -0300

    [system] batteries monitor

--- nixos/home/hikari.nix
+++ nixos/home/hikari.nix
@@ -173,6 +173,20 @@ in
           RestartSec = 1;
         };
       };
+
+      batteries-monitor = {
+        Unit = {
+          Description = "Batteries Monitor";
+        };
+
+        Service = {
+          Type = "exec";
+          ExecStart = "${config.home.homeDirectory}/.local/bin/battery monitor";
+          Restart = "always";
+          RestartSec = 1;
+        };
+      };
+
     };
   };
 }
