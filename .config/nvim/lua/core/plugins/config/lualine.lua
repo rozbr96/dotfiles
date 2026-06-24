c0aacf3e73aeac3970e23041187dd901b5fe0667
Rosemilson <rozbr96@gmail.com>
2026-06-24 17:25:15 -0300

    [nvim] minor lualine adjustment

--- .config/nvim/lua/core/plugins/config/lualine.lua
+++ .config/nvim/lua/core/plugins/config/lualine.lua
@@ -34,7 +34,7 @@ require 'lualine'.setup {
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = { 'vim.fn.expand("%:.")' },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
