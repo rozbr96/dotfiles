515c3736c3f8eddc00ac800475a268805a104907
Rosemilson <rozbr96@gmail.com>
2026-08-29 13:53:50 -0300

    [dev] python coding qol

--- .config/nvim/lua/core/plugins/config/arborist.lua
+++ .config/nvim/lua/core/plugins/config/arborist.lua
@@ -1,1 +1,5 @@
-require('arborist').setup {}
+require('arborist').setup {
+  disable = {
+    indent = { "python" },
+  },
+}
--- .config/nvim/lua/core/plugins/config/conform.lua
+++ .config/nvim/lua/core/plugins/config/conform.lua
@@ -1,5 +1,7 @@
 require('conform').setup {
-  formatters_by_ft = {},
+  formatters_by_ft = {
+    python = { "ruff_format" },
+  },
 
   format_on_save = {
     timeout_ms = 500,
